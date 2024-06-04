c66038eff03b145dcd315099d0e094a57379f9e5
Nayan-Bhisara <bhisaranayan3196@gmail.com>
2024-06-04 13:37:03 +0530

    Pr-6

--- Pr-6/src/Todo.jsx
+++ Pr-6/src/Todo.jsx
@@ -1,87 +1,113 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { FiEdit } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaPlus } from "react-icons/fa6";
+import "./todo.css"
 
 const Todo = () => {
+  const [task, setTask] = useState("");
+  const [title, setTitle] = useState("");
+  let data = localStorage.getItem("user")
+? JSON.parse(localStorage.getItem("user"))
+: [];
+  const [record, setRecord] = useState(data);
+  const [editId, setEditId] = useState("");
+  const [single, setSingle] = useState(null);
 
-  const [task, setTask] = useState("")
-  const [title, setTitle] = useState("")
-  let data = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : []
-  const [record, setRecord] = useState(data)
-  const [edit, setEdit] = useState("")
+  
 
-  const handleSubmite = (e) => {
-    e.preventDefault()
-
-    let obj = { 
-      id: Date.now(), task, title 
+  useEffect(() => {
+    if (single) {
+      setTask(single.task);
+      setTitle(single.title);
+    } else {
+      setTask("");
+      setTitle("");
     }
+  }, [single]);
 
-    if (edit) {
-      let all = [...record]
-      let editdatas = all.map((val) => {
-        if (val.id == edit) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let obj = { id: Date.now(), task, title };
+
+    if (editId) {
+      let updatedRecords = record.map((val) => {
+        if (val.id === editId) {
           return {
             ...val,
-            task: task,
-            title: title,
-          }
+            task,
+            title,
+          };
         }
         return val;
       });
-      setRecord(editdatas)
-      localStorage.setItem("user", JSON.stringify(editdatas))
-
-      setEdit("")
+      setRecord(updatedRecords);
+      localStorage.setItem("user", JSON.stringify(updatedRecords));
+      setEditId("");
+      setSingle(null);
     } else {
-      let newlist = [...record, obj]
-      localStorage.setItem("user", JSON.stringify(newlist))
-      setRecord(newlist)
+      let all = [...record, obj];
+      setRecord(all);
+      localStorage.setItem("user", JSON.stringify(all));
     }
-    setTask("")
-    setTitle("")
-  }
+    setTask("");
+    setTitle("");
+  };
 
   const handleEdit = (id) => {
-    let all = [...record]
-    let editdata = all.find(val => val.id == id)
-    setEdit(id)
-    setTitle(editdata.title)
-    setTask(editdata.task)
-  }
+    let editData = record.find((val) => val.id === id);
+    setEditId(id);
+    setSingle(editData);
+  };
 
-  const handleDelet = (id) => {
-    let all = [...record]
-    let deletdata = all.filter((val => val.id != id))
-    setRecord(deletdata)
-    localStorage.setItem("user",JSON.stringify(deletdata))
-  }
-
+  const handleDelete = (id) => {
+    let updatedRecords = record.filter((val) => val.id !== id);
+    setRecord(updatedRecords);
+    localStorage.setItem("user", JSON.stringify(updatedRecords));
+  };
 
   return (
-    <div align="center">
-      <div style={{width:"400px",border:"4px solid #82E0AA",borderRadius:"6px"}}>
+    <div className="todo" align="center">
+      <div className="todomain">
         <h1>TODO App</h1>
-        <form onSubmit={handleSubmite}>
-              Title : {""} <input type="text" placeholder='Add Title' onChange={(e) => setTitle(e.target.value)} value={title} />
-              Task : {""} <input type="text" placeholder='Add New Task' onChange={(e) => setTask(e.target.value)} value={task} />
-              <input type="submit" />
-              {
-                record.map((val) => {
-                  return (
-                    <div key={val.id}>
-                      <h3>{val.title}</h3>
-                      <p>{val.task}</p>
-                      <button onClick={() => handleEdit(val.id)}><FiEdit /></button>
-                      <button onClick={() => handleDelet(val.id)}><RiDeleteBin6Line /></button>
-                    </div>
-                  )
-                })
-              }
+        <form onSubmit={handleSubmit}>
+          <div className="todocontent">
+            <input className="todotitle"
+              type="text"
+              placeholder="Add Title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            /><br></br><br></br>
+            <input className="todotask"
+              type="text"
+              placeholder="Add New Task"
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
+            />
+            <button className="todobtn" type="submit"><FaPlus /></button>
+          </div>
         </form>
+        <h4>Tasks</h4>
+        {record.map((val) => (
+          <div className="todonotes" key={val.id}>
+            <div className="notescontent">
+              <h3>{val.title}</h3>
+              <p>{val.task}</p>
+            </div>
+            <div className="notesbtn">
+              <button onClick={() => handleEdit(val.id)}>
+                <FiEdit />
+              </button>
+              <button onClick={() => handleDelete(val.id)}>
+                <RiDeleteBin6Line />
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
